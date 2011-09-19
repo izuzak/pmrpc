3b9c264b61d47ec21121d7905dabe1b108acfd64
izuzak <izuzak@gmail.com>
2011-09-19 08:03:46 +0200

    expanding hello worker world example. fixes #1

--- examples/hello-worker-world/worker.js
+++ examples/hello-worker-world/worker.js
@@ -2,5 +2,11 @@ importScripts('https://raw.github.com/izuzak/pmrpc/master/pmrpc.js');
 
 // expose a procedure
 pmrpc.register( {
-  publicProcedureName : "HelloPMRPC",
+  publicProcedureName : "HelloPMRPC_1",
   procedure : function(printParam) { return printParam; } } );
+  
+// invoke remote procedure
+setTimeout(function() {
+  pmrpc.call( {
+    publicProcedureName : "HelloPMRPC_2",
+    params : ["Hello World (from worker)!"] } ); }, 1000);
